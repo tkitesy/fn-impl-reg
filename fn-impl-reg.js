1f429bca9e499615b2e6ea4e50f5b005e53252a0
Yang Jun <yangjun23@163.com>
2021-03-17 22:02:39 +0800

    create

---
+++ fn-impl-reg.js
@@ -0,0 +1,49 @@
+const alpha = (ch) => (str, index, succ, fail) =>
+  ch === str[index] ? succ(str, index + 1) : fail(str, index);
+
+const epsilon = () => (str, index, succ, fail) => succ(str, index);
+
+const concat = (head, tail) => (str, index, succ, fail) =>
+  head(str, index, (s, i) => tail(s, i, succ, fail), fail);
+
+const alter = (head, tail) => (str, index, succ, fail) =>
+  head(str, index, succ, (s, i) => tail(str, index, succ, fail));
+
+const kleene = (m) => (str, index, succ, fail) =>
+  m(str, index, (s, i) => kleene(m)(s, i, succ, fail), succ);
+
+const end = () => (str, index, succ, fail) =>
+  str.length === index ? succ(str, index) : fail(str, index);
+
+const log = (prefix) => (str, index) => {
+  console.log(
+    `${str}
+${new Array(index).fill(" ").join("")}^   --${prefix}`
+  );
+};
+
+const test = (name, m, ...strs) => {
+  console.log(`==test: ${name} =================================`);
+  strs.forEach((str) => m(str, 0, log("success"), log("fail")));
+  console.log(`=================================================`);
+};
+
+const seq = (f) => (m1, ...m2) => m2.reduce((prev, curr) => f(prev, curr), m1);
+
+const m1 = concat(alpha("a"), alpha("b"));
+const m2 = seq(concat)(alpha("a"), alpha("b"), alpha("c"));
+const m3 = kleene(m2);
+const m4 = seq(alter)(alpha("d"), alpha("e"));
+const m6 = seq(alter)(alpha("f"), epsilon());
+
+const m = seq(concat)(m1, m3, m4, m6, end());
+
+test(
+  "/^ab(abc)*(d|e)f?$/",
+  m,
+  "ababcabcabc",
+  "ababcabcabce",
+  "ababcabcabcf",
+  "ababcabcabcd",
+  "ababcabcabcdf",
+);
